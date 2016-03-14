7665458aabc0ec87b09a9d2613c30f669c700ab2
Santiago Ramirez Ayuso <santiago.ramirez@mulesoft.com>
2016-03-14 09:50:13 -0300

    Add alignment of particles in grid

--- cim.rb
+++ cim.rb
@@ -1,65 +1,98 @@
 #!/usr/bin/env ruby
 
-class Grid
-  attr_accessor :particles
+require 'pp'
+require 'set'
 
-  def initialize(grid_size, particle_number, particles)
+class State
+  attr_reader :cell_size
+  attr_accessor :particles, :grid
+
+  def initialize(time, grid_size, cell_size, particle_number, particles)
+    @time = time
     @grid_size = grid_size
+    @cell_size = cell_size
+    @grid = Hash.new
     @particle_number = particle_number
     @particles = particles
   end  
 end
 
 class Particle
-  attr_writer :x, :y
+  attr_reader :id, :color, :radius
+  attr_accessor :x, :y
+
+  @@ids = 0
 
   def initialize(radius, color)
+      @@ids += 1
+      @id = @@ids
       @radius = radius
       @color = color
       @x = nil
       @y = nil
   end
 
-  def to_s
-    "radius: #{@radius}, color: #{@color}"
+  def to_s()
+    "id: #{@id}, radius: #{@radius}, color: #{@color}, x: #{@x}, y: #{@y}"
   end
 end
 
-def parse_static(filename)
+def parse_input(static_filename, dynamic_filename)
+  time = nil
   grid_size = nil
   particle_number = nil
   particles = []
 
-  File.open(filename).each_with_index do |line, index|
-    particle_number = Integer(line) if index == 0
-    grid_size = Integer(line) if index == 1
-    particles.push(Particle.new(line.split(" ").first, line.split(" ").last)) if index > 1
+  File.open(static_filename).each_with_index do |line, index|
+    particle_number = line.to_i if index == 0
+    grid_size = line.to_i if index == 1
+    particles.push(Particle.new(line.split(" ").first.to_f, line.split(" ").last.to_i)) if index > 1
   end
 
-  return Grid.new(grid_size, particle_number, particles)
-end
-
-def parse_dynamic(filename, grid)
-  i = 0
-  time = nil
-  particles = Hash.new
-
-  File.open(filename).each_with_index do |line, index|
+  File.open(dynamic_filename).each_with_index do |line, index|
     if index == 0
-      time = Integer(line)
-      particles[time] = []
+      time = line.to_i
     else
-      p = grid.particles[i]
-      p.x = Float(line.split(" ").first)
-      p.y = Float(line.split(" ").last)
-      particles[time].push(p)
-      i += 1
+      particles[index - 1].x = line.split(" ").first.to_f
+      particles[index - 1].y = line.split(" ").last.to_f
     end
   end
 
-  grid.particles = particles
+  return State.new(time, grid_size, 1, particle_number, particles) # Change this to return array of states, one for each time
 end
 
-grid = parse_static("Static100.txt")
-parse_dynamic("Dynamic100.txt", grid)
-puts grid.particles
+def align_grid(state)
+
+  state.particles.each do |particle|
+    x = particle.x
+    y = particle.y
+    radius = particle.radius
+
+    state.grid = add_particle(state.grid, state.cell_size, x, y, particle)
+
+    state.grid = add_particle(state.grid, state.cell_size, x - radius, y, particle)
+    state.grid = add_particle(state.grid, state.cell_size, x, y - radius, particle)
+    state.grid = add_particle(state.grid, state.cell_size, x + radius, y, particle)
+    state.grid = add_particle(state.grid, state.cell_size, x, y + radius, particle)
+
+    state.grid = add_particle(state.grid, state.cell_size, x + (0.5 * radius), y + (0.5 * radius), particle)
+    state.grid = add_particle(state.grid, state.cell_size, x - (0.5 * radius), y - (0.5 * radius), particle)
+    state.grid = add_particle(state.grid, state.cell_size, x - (0.5 * radius), y + (0.5 * radius), particle)
+    state.grid = add_particle(state.grid, state.cell_size, x + (0.5 * radius), y - (0.5 * radius), particle)
+  end
+
+  return state
+end
+
+def add_particle(grid, cell_size, x, y, particle)
+  grid[[grid_position(x, cell_size), grid_position(y, cell_size)]] = Set.new if grid[[grid_position(x, cell_size), grid_position(y, cell_size)]] == nil
+  grid[[grid_position(x, cell_size), grid_position(y, cell_size)]].add(particle)
+  return grid
+end
+
+def grid_position(pos, cell_size)
+  return (pos/cell_size).floor
+end
+
+state = parse_input("Static100.txt", "Dynamic100.txt")
+pp align_grid(state).grid
